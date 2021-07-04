7f604021256bf8175c63d11e5a320de6193212c2
Pleon88 <pablo.leon.88@gmail.com>
2021-07-03 22:18:17 -0400

    Layout_login user

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -3,7 +3,7 @@ const Model = require("../models");
 
 const showAll = async (req, res) => {
   const usuarios = await Model.Usuario.findAll();
-  return res.render('users', {layout: './Shared/layout', users: usuarios })
+  return res.render('users', {layout: './Shared/layout_login', users: usuarios, user: req.user })
   // return res.json({ data: usuarios})
 };
 
